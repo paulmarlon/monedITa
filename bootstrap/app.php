c4195c14f2abf8913405d622678918ddaa36a1cd
paul quispe <paul79ok@gmail.com>
2026-09-22 13:42:32 -0400

    Fix: remover eliminar 44

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use \Illuminate\Session\Middleware\StartSession;
+
 use Illuminate\Http\Request;
 use App\Http\Middleware\CheckInactivity;
 
@@ -19,6 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         // 2. Agrégalo al grupo web para que vigile todas las rutas de la aplicación
         $middleware->web(append: [
+            StartSession::class,
             CheckInactivity::class,
         ]);
     })
